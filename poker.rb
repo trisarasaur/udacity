4d4a37db6cce163e962d43cdf5cb792ff8a60a43
Sara Snider <trisarasaur@gmail.com>
2013-05-30 19:18:32 -0600

    Finish pulling out Hand class

--- poker.rb
+++ poker.rb
@@ -50,12 +50,12 @@ end
 
 
 def poker hands
-  ranked = hands.sort_by {|h| hand_rank(h)}.reverse
+  ranked = hands.sort_by {|h| h.hand_rank}.reverse
   first_winner = ranked[0]
   winners = []
 
   ranked.each do |hand|
-    if Hand.new(hand).card_values == Hand.new(first_winner).card_values
+    if hand.card_values == first_winner.card_values
       winners << hand
     end
   end
@@ -65,8 +65,14 @@ end
 
 class Hand
 
+  attr_reader :cards
+
   def initialize cards
     @cards = cards
+  end
+
+  def <=> hand
+    self.cards <=> hand.cards
   end
 
   def card_values
@@ -77,116 +83,115 @@ class Hand
     end
     values.map { |v| lookup[v] }.sort.reverse
   end
-end
 
-def hand_rank hand
-  my_hand = Hand.new(hand)
+  def hand_rank 
 
-  if straight_flush?(hand)
-    [8, my_hand.card_values.max]
+    if straight_flush?
+      [8, card_values.max]
 
-  elsif four_of_a_kind?(hand)
-    [7, four_of_a_kind(hand), one_of_a_kind(hand)]
+    elsif four_of_a_kind?
+      [7, four_of_a_kind, one_of_a_kind]
 
-  elsif full_house?(hand)
-    [6, three_of_a_kind(hand), two_of_a_kind(hand)]
+    elsif full_house?
+      [6, three_of_a_kind, two_of_a_kind]
 
-  elsif flush?(hand)
-    [5] + my_hand.card_values
+    elsif flush?
+      [5] + card_values
 
-  elsif straight?(hand)
-    [4, my_hand.card_values.max]
+    elsif straight?
+      [4, card_values.max]
 
-  elsif three_of_a_kind?(hand)
-    [3, three_of_a_kind(hand)] + my_hand.card_values
+    elsif three_of_a_kind?
+      [3, three_of_a_kind] + card_values
 
-  elsif two_pair?(hand)
-    [2] + two_pair(hand) + my_hand.card_values
+    elsif two_pair?
+      [2] + two_pair + card_values
 
-  elsif one_pair?(hand)
-    [1, two_of_a_kind(hand)] + my_hand.card_values
+    elsif one_pair?
+      [1, two_of_a_kind] + card_values
 
-  else 
-    [0] + my_hand.card_values
-  end
-end
-
-
-def number_of_kind number, values
-  counted = values.group_by { |v| v }
-  kind = nil
-  counted.each do |k,v| 
-       kind = k if v.length == number
+    else 
+      [0] + card_values
     end
-  kind 
-end
-
-def four_of_a_kind hand
-  number_of_kind(4, Hand.new(hand).card_values)
-end
-
-def three_of_a_kind hand
-  number_of_kind(3, Hand.new(hand).card_values)
-end
-
-def two_of_a_kind hand
-  number_of_kind(2, Hand.new(hand).card_values)
-end
-
-def one_of_a_kind hand
-  number_of_kind(1, Hand.new(hand).card_values)
-end
-
-def two_pair hand
-  values = Hand.new(hand).card_values
-  
-  first_pair = number_of_kind(2, values)
-  second_pair = number_of_kind(2, values.reverse)
-  
-  if first_pair != second_pair
-    [first_pair, second_pair].sort.reverse
-  else
-    nil
   end
-end
 
-def straight_flush? hand
-  straight?(hand) && flush?(hand) 
-end
 
-def four_of_a_kind? hand
-  number_of_kind(4, Hand.new(hand).card_values) != nil 
-end
-
-def full_house? hand
-  number_of_kind(3, Hand.new(hand).card_values) != nil && number_of_kind(2, Hand.new(hand).card_values) !=nil  
-end
-
-def flush? hand
-  suits = hand.map { |card| card[1] }
-  if suits.uniq.length == 1
-    true
-  else
-    false
+  def number_of_kind number, values
+    counted = values.group_by { |v| v }
+    kind = nil
+    counted.each do |k,v| 
+         kind = k if v.length == number
+      end
+    kind 
   end
-end
 
-def straight? hand
-  values = Hand.new(hand).card_values
-  (values.last..values.first).to_a.reverse == values 
-end
+  def four_of_a_kind
+    number_of_kind(4, card_values)
+  end
 
-def three_of_a_kind? hand
-  number_of_kind(3, Hand.new(hand).card_values) != nil
-end
+  def three_of_a_kind 
+    number_of_kind(3, card_values)
+  end
 
-def two_pair? hand
-  two_pair(hand) != nil 
-end
+  def two_of_a_kind
+    number_of_kind(2, card_values)
+  end
 
-def one_pair? hand
-  number_of_kind(2, Hand.new(hand).card_values) != nil
-end
+  def one_of_a_kind
+    number_of_kind(1, card_values)
+  end
 
+  def two_pair
+    values = card_values
+    
+    first_pair = number_of_kind(2, values)
+    second_pair = number_of_kind(2, values.reverse)
+    
+    if first_pair != second_pair
+      [first_pair, second_pair].sort.reverse
+    else
+      nil
+    end
+  end
+
+  def straight_flush?
+    straight? && flush?
+  end
+
+  def four_of_a_kind?
+    number_of_kind(4, card_values) != nil 
+  end
+
+  def full_house?
+    number_of_kind(3, card_values) != nil && number_of_kind(2, card_values) !=nil  
+  end
+
+  def flush?
+    suits = @cards.map { |card| card[1] }
+    if suits.uniq.length == 1
+      true
+    else
+      false
+    end
+  end
+
+  def straight? 
+    values = card_values
+    (values.last..values.first).to_a.reverse == values 
+  end
+
+  def three_of_a_kind? 
+    number_of_kind(3, card_values) != nil
+  end
+
+  def two_pair?
+    two_pair != nil 
+  end
+
+  def one_pair? 
+    number_of_kind(2, card_values) != nil
+  end
+
+end
 
 
--- test/poker_test.rb
+++ test/poker_test.rb
@@ -39,12 +39,12 @@ end
 describe 'poker game' do
 
   before do
-    @straight_flush = "6C 7C 8C 9C TC".split 
-    @four_kind = "9D 9H 9S 9C 7D".split 
-    @full_house = "TD TC TH 7C 7D".split 
-    @two_pair = "9D 9H 6C 6D AH".split 
-    @other_two_pair = "9C 9S 6H 6S AD".split
-    @more_two_pair = "9D 9C 6D 6H AS".split 
+    @straight_flush = Hand.new("6C 7C 8C 9C TC".split )
+    @four_kind = Hand.new("9D 9H 9S 9C 7D".split )
+    @full_house = Hand.new("TD TC TH 7C 7D".split )
+    @two_pair = Hand.new("9D 9H 6C 6D AH".split )
+    @other_two_pair = Hand.new("9C 9S 6H 6S AD".split)
+    @more_two_pair = Hand.new("9D 9C 6D 6H AS".split )
   end
 
   it 'gives correct winning hand with three hands' do
@@ -80,16 +80,16 @@ end
 
 describe 'straight' do 
   before do
-    @straight = "9C 8H 7D 6S 5C".split
-    @not_straight = "9C 8H 8D 6S 5C".split
+    @straight = Hand.new("9C 8H 7D 6S 5C".split)
+    @not_straight = Hand.new("9C 8H 8D 6S 5C".split)
   end
 
   it 'can tell if a hand is a straight' do
-    straight?(@straight).must_equal true
+    @straight.straight?.must_equal true
   end
 
   it 'can tell if a hand is not a straight' do
-    straight?(@not_straight).must_equal false
+    @not_straight.straight?.must_equal false
   end
 
 end
@@ -98,16 +98,16 @@ end
 describe 'flush' do
 
   before do
-    @straight_flush = "6C 7C 8C 9C TC".split 
-    @four_kind = "9D 9H 9S 9C 7D".split 
+    @straight_flush = Hand.new("6C 7C 8C 9C TC".split)
+    @four_kind = Hand.new("9D 9H 9S 9C 7D".split)
   end
   
   it 'can tell if a hand is a flush' do
-    flush?(@straight_flush).must_equal true
+    @straight_flush.flush?.must_equal true
   end
 
   it 'can tell if a hand is not a flush' do
-    flush?(@four_kind).must_equal false
+    @four_kind.flush?.must_equal false
   end
 
 end
@@ -116,16 +116,16 @@ end
 describe 'two pair' do
 
   before do
-    @four_kind = "9D 9H 9S 9C 7D".split 
-    @two_pair = "9D 9H 6C 6D AH".split 
+    @four_kind = Hand.new("9D 9H 9S 9C 7D".split )
+    @two_pair = Hand.new("9D 9H 6C 6D AH".split )
   end
   
   it 'gives a list of the pairs when given a hand with two pair' do
-    two_pair(@two_pair).must_equal [9,6]
+    @two_pair.two_pair.must_equal [9,6]
   end
 
   it 'returns nil when a hand is not a two pair' do
-    two_pair(@four_kind).must_equal nil
+    @four_kind.two_pair.must_equal nil
   end
 
 end
@@ -134,24 +134,24 @@ end
 describe 'number of a kind' do
 
   before do
-    @four_kind = "9D 9H 9S 9C 7D".split 
-    @fk_ranks = Hand.new(@four_kind).card_values
+    @four_kind = Hand.new("9D 9H 9S 9C 7D".split)
+    @fk_ranks = @four_kind.card_values
   end
 
   it 'can tell if a hand contains four of a kind' do
-    number_of_kind(4, @fk_ranks).must_equal 9
+    @four_kind.number_of_kind(4, @fk_ranks).must_equal 9
   end
 
   it 'can tell if a hand is does not contain three of a kind' do
-    number_of_kind(3, @fk_ranks).must_equal nil
+    @four_kind.number_of_kind(3, @fk_ranks).must_equal nil
   end
 
   it 'can tell if a hand does not contain two of a kind' do
-    number_of_kind(2, @fk_ranks).must_equal nil
+    @four_kind.number_of_kind(2, @fk_ranks).must_equal nil
   end
 
   it 'can tell if a hand contains only one of a kind' do
-    number_of_kind(1, @fk_ranks).must_equal 7
+    @four_kind.number_of_kind(1, @fk_ranks).must_equal 7
   end
 
 end
@@ -160,51 +160,51 @@ end
 describe 'hand rankings' do
 
   before do
-    @straight_flush = "6C 7C 8C 9C TC".split 
-    @four_kind = "9D 9H 9S 9C 7D".split 
-    @full_house = "TD TC TH 7C 7D".split 
-    @flush = "AS JS TS 6S 3S".split
-    @straight = "9S 8D 7S 6H 5C".split
-    @three_kind = "4C 4H 4D QS 2S".split
-    @two_pair = "9D 9H 6C 6D AH".split  
-    @one_pair = "2D 2H QH 7H 6C".split
-    @high_card = "AH KH QC TH 2C".split
+    @straight_flush = Hand.new("6C 7C 8C 9C TC".split )
+    @four_kind = Hand.new("9D 9H 9S 9C 7D".split )
+    @full_house = Hand.new("TD TC TH 7C 7D".split )
+    @flush = Hand.new("AS JS TS 6S 3S".split)
+    @straight = Hand.new("9S 8D 7S 6H 5C".split)
+    @three_kind = Hand.new("4C 4H 4D QS 2S".split)
+    @two_pair = Hand.new("9D 9H 6C 6D AH".split  )
+    @one_pair = Hand.new("2D 2H QH 7H 6C".split)
+    @high_card = Hand.new("AH KH QC TH 2C".split)
   end
   
   it 'knows the hand rank of a straight flush'  do
-    hand_rank(@straight_flush).must_equal [8,10]
+    @straight_flush.hand_rank.must_equal [8,10]
   end
 
   it 'knows the hand rank of four of a kind' do
-    hand_rank(@four_kind).must_equal [7,9,7]
+    @four_kind.hand_rank.must_equal [7,9,7]
   end
 
   it 'knows the hand rank of a full house' do
-    hand_rank(@full_house).must_equal [6, 10, 7]
+    @full_house.hand_rank.must_equal [6, 10, 7]
   end
 
   it 'knows the hand rank of a flush' do
-    hand_rank(@flush).must_equal [5, 14, 11, 10, 6, 3]
+    @flush.hand_rank.must_equal [5, 14, 11, 10, 6, 3]
   end
 
   it 'knows the hand rank of a straight' do
-    hand_rank(@straight).must_equal [4, 9]
+    @straight.hand_rank.must_equal [4, 9]
   end
 
   it 'knows the hand rank of three of a kind' do
-    hand_rank(@three_kind).must_equal [3, 4, 12, 4, 4, 4, 2] 
+    @three_kind.hand_rank.must_equal [3, 4, 12, 4, 4, 4, 2] 
   end
 
   it 'knows the hand rank of a two pair' do
-    hand_rank(@two_pair).must_equal [2, 9, 6, 14, 9, 9, 6, 6]
+    @two_pair.hand_rank.must_equal [2, 9, 6, 14, 9, 9, 6, 6]
   end
 
   it 'knows the hand rank of a one pair' do
-    hand_rank(@one_pair).must_equal [1 , 2, 12, 7, 6, 2, 2]
+    @one_pair.hand_rank.must_equal [1 , 2, 12, 7, 6, 2, 2]
   end
 
   it 'knows the hand rank of a high card hand' do
-    hand_rank(@high_card).must_equal [0, 14, 13, 12, 10, 2]
+    @high_card.hand_rank.must_equal [0, 14, 13, 12, 10, 2]
   end
 
 end
